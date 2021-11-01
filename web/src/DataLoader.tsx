fe33e095d26a2c918b5dc93d865e2f5d567bbde5
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-01 13:36:15 -0300

    show loading

--- web/src/DataLoader.tsx
+++ web/src/DataLoader.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import MiniSearch from 'minisearch'
+import fetchProgress from 'fetch-progress'
 
 import Loading from './Loading'
 import SearchBox from './SearchBox'
@@ -39,21 +40,63 @@ const range = function(from: number, to: number) {
 
 const DataLoader : React.FunctionComponent<DataLoaderProps> = () => {
   const [minisearches, setMinisearches] = useState<MiniSearch[]>([])
+  const [progress, setProgress] = useState<number>()
+
   useState(() => {
     (async () => {
+      const totalRequestsSize = range(0, NUM_MINISEARCH).map(() => 0)
+      const requestsTransferredSize = range(0, NUM_MINISEARCH).map(() => 0)
+      const updateProgress = () => {
+        if (totalRequestsSize.some((x) => x === 0)) return
+        setProgress(
+          requestsTransferredSize.reduce((a, b) => a + b, 0) /
+          totalRequestsSize.reduce((a, b) => a + b, 0)
+        )
+      }
       setMinisearches(await Promise.all(range(0, NUM_MINISEARCH)
         // TODO: save to localStorage
-        .map((i) => fetch(`/data/index${i}.json`)
-            .then((res) => res.text())
-            .then((text) => MiniSearch.loadJSON(text, MS_CONFIG))
-          )
-      ))
+        .map((i) => {
+          let headers = new Headers();
+          // appending range header to get number of bytes
+          // since chunked transfer encoding does not return content length
+          headers.append("Range", "bytes=0-" + (100 * 1024 * 1024));
+          let request = new Request(`/data/index${i}.json`, {headers:headers});
+          return fetch(request)
+            .then((r) => {
+              const range = r.headers.get('content-range')
+              let reqSize = 0;
+              if (range && range.includes('/')) {
+                reqSize = parseInt(range.split('/')[1])
+                totalRequestsSize[i] = reqSize
+                updateProgress()
+              }
+              return fetchProgress({
+                onProgress(progress) {
+                  requestsTransferredSize[i] = progress.transferred
+                  updateProgress()
+                },
+                onError(err) {
+                  // TODO: handle error
+                  console.error(err);
+                },
+              })(r)
+            })
+            .then((res) => res.blob())
+            .then(async (blob) => {
+              requestsTransferredSize[i] = blob.size
+              updateProgress()
+              return MiniSearch.loadJSON(await blob.text(), MS_CONFIG)
+            })
+            // TODO: handle error
+          })
+        )
+      )
     })()
   })
 
   return (
     <>
-      {minisearches.length === 0 ? <Loading /> : <SearchBox minisearches={minisearches} />}
+      {minisearches.length === 0 ? <Loading progress={progress} /> : <SearchBox minisearches={minisearches} />}
     </>
   )
 }
--- web/src/Loading.tsx
+++ web/src/Loading.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Loading() {
+function Loading({ progress }: { progress: number | undefined }) {
   return (
-    <p>Loading...</p>
+    <p>
+      Loading...
+      <br />
+      {progress !== undefined && <input type="range" value={progress * 1000} readOnly name="volume" min="0" max="1000" step="1" />}
+    </p>
   )
 }
 
