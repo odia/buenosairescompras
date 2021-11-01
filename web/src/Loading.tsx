47c3492f6a9f0e4375b5de39756e2a54eb6fd2ba
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-01 13:50:39 -0300

    show undeterminated loading status

--- web/src/Loading.tsx
+++ web/src/Loading.tsx
@@ -6,6 +6,7 @@ function Loading({ progress }: { progress: number | undefined }) {
       Loading...
       <br />
       {progress !== undefined && <input type="range" value={progress * 1000} readOnly name="volume" min="0" max="1000" step="1" />}
+      {progress === undefined && 'unknown length'}
     </p>
   )
 }
