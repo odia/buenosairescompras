b16192b3120e77f19943c85ee44b14bc67b1ac7a
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-10 19:20:02 -0300

    Fix bad import

--- shared/lib.js
+++ shared/lib.js
@@ -1,2 +1,24 @@
-const { MS_CONFIG, COLS, NUM_MINISEARCH } = require('./minisearch_config')
+const NUM_MINISEARCH = 10
+const COLS = [
+  'tender/title',
+  'tender/description',
+  'tender/status',
+  'tender/value/currency',
+  'tender/value/amount',
+  'tender/procuringEntity/name',
+  'tender/procurementMethodDetails',
+  'tender/mainProcurementCategory',
+  'tender/tenderPeriod/startDate',
+  'tender/tenderPeriod/endDate',
+  'tender/tenderPeriod/durationInDays',
+  'tender/enquiryPeriod/startDate',
+  'tender/enquiryPeriod/endDate',
+  'tender/enquiryPeriod/durationInDays',
+  'tender/documents/0/url',
+]
+
+const MS_CONFIG = {
+  fields: ['tender/title', 'tender/description', 'content'],
+  storeFields: COLS.concat(['compressed']),
+}
 module.exports = { MS_CONFIG, COLS, NUM_MINISEARCH }
