1674f71950fdf2bf016c40ea89d6f977f2f50c66
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-12 06:50:46 -0300

    Add progress bar for index generation

--- create-index/main.js
+++ create-index/main.js
@@ -5,6 +5,7 @@ const htmlparse = require('node-html-parser').parse;
 const { compress } = require('lz-string')
 const MiniSearch = require('minisearch')
 const { MS_CONFIG, COLS, NUM_MINISEARCH } = require('bac-shared')
+const cliProgress = require('cli-progress');
 
 const range = function*(from,to) {
     for(let i = from; i < to; i++) yield i;
@@ -13,20 +14,29 @@ const range = function*(from,to) {
 const PATH = process.argv[2] === 'sample' ? '../data/sample.csv' : '../data/releases_documents_items.csv'
 const minisearches = [...range(0, NUM_MINISEARCH)].map(() => new MiniSearch(MS_CONFIG))
 
-const urls = []
 let i = 0
+const urls = []
+
+console.log('Generating index...')
+const numBytes = fs.statSync(PATH).size;
+const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+progressBar.start(numBytes, 0)
 
 fs.createReadStream(PATH)
   .pipe(csvparse({ columns: true }))
   .on('error', console.error)
   .on('data', (row) => {
+    progressBar.increment(Object.values(row).join(',').length) // approximation of row bytes length :see_no_evil:
+
     const url = row['tender/documents/0/url']
     if (url === '') return;
     if (urls.includes(url)) return
     urls.push(url)
     const doc = Object.fromEntries(Object.entries(row).filter(([k, v]) => COLS.includes(k)))
+
     const f = '../data/' + url.replace(/\//g, '_')
-    const html = fs.readFileSync(f, 'utf8')
+    const html = fs.readFileSync(f)
+
     const dom = htmlparse(html)
     const relevant = dom.querySelector('div#divImprimir')
     dom.querySelectorAll('script').forEach((s) => s.remove())
@@ -36,6 +46,8 @@ fs.createReadStream(PATH)
     minisearches[i++ % NUM_MINISEARCH].add(doc)
   })
   .on('end', async () => {
+    progressBar.update(numBytes)
+    progressBar.stop()
     const sizes = []
     await Promise.all(minisearches.map(async (ms, i) => {
       const str = JSON.stringify(ms.toJSON())
