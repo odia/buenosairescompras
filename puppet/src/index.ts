3933b196313b4c0f1d3ccda15e0b4afa43e3ffaa
Niv Sardi <xaiki@evilgiggle.com>
2022-01-28 16:50:02 -0300

    simple parser
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- puppet/src/index.ts
+++ puppet/src/index.ts
@@ -1,4 +1,17 @@
 import Runner from './runner';
+import { writeFile } from 'fs';
 
 const puppet = new Runner();
+puppet.on('data', d => {
+  const nump = d.VistaPreviaPliego.CabeceraPliego.NumPliego;
+  writeFile(`${__dirname}/../data/${nump}.json`,
+    JSON.stringify(d, null, 2), err => {
+      if (err) return console.error(`couldn't write file data/${nump}.json`);
+      console.log(`wrote data/${nump}.json`);
+
+    })
+});
+puppet.on('done', () => {
+  puppet.close();
+});
 puppet.run();
--- puppet/src/runner.ts
+++ puppet/src/runner.ts
@@ -1,23 +1,28 @@
 import { Browser, Page } from 'puppeteer-core';
 import selectors from './selectors';
 
+const EventEmitter = require('events');
 const debug = require('debug')('puppet');
 
 const BROWSERLESS_HOST = process.env.BROWSERLESS_HOST || 'localhost';
 const BROWSERLESS_PORT = process.env.BROWSERLESS_PORT || '3000';
 const BASE_URL = process.env.BASE_URL || 'https://www.buenosairescompras.gob.ar'
 
-export default class Runner {
+export default class Runner extends EventEmitter {
   target: string;
   browser: Browser | undefined;
   page: Page | undefined;
 
   constructor() {
+    super();
     this.target = `ws://${BROWSERLESS_HOST}:${BROWSERLESS_PORT}`;
     debug(`will connect to ${this.target}`);
   }
+  close() {
+    if (this.browser) this.browser.close();
+  }
   async connect() {
-    this.browser = await require('puppeteer').connect({ browserWSEndpoint: this.target })
+    this.browser = this.browser || await require('puppeteer').connect({ browserWSEndpoint: this.target })
       .catch(() => {
         debug(`
 COULD NOT CONNECT TO BROWSERLESS
@@ -30,12 +35,36 @@ will try to spawn a chromedriver instance for you to debug`)
       debug("couldn't init Browser");
       return null;
     };
-    this.page = await this.browser.newPage();
+    this.page = this.page || await this.browser.newPage();
+    this.emit("ready")
   }
   async wait() {
     if (!this.page) return null;
     return await this.page.waitForNetworkIdle();
   }
+  process(data) {
+    const p = data.reduce((a, e) => {
+      if (!e[1]) return a;
+
+      const [_, cat, field, label] =
+        e[0].match(/.*_UC([^_]+)_(?:UC_|usr)?([^_]*)_(?:lbl|rpt|gv)?([^_]+)/);
+
+      if (!a[cat]) a[cat] = {};
+      if (!a[cat][field]) a[cat][field] = {};
+      if (!a[cat][field][label]) {
+        a[cat][field][label] = e[1];
+      } else if (!(a[cat][field][label] instanceof Array)) {
+        a[cat][field][label] = [a[cat][field][label], e[1]];
+      } else {
+        a[cat][field][label].push(e[1])
+      }
+
+      return a;
+    }, {})
+    this.emit("data", p);
+    return p;
+  }
+
   public async run() {
     await this.connect();
 
@@ -50,28 +79,30 @@ will try to spawn a chromedriver instance for you to debug`)
     /* required as browsing directly seems to fail */
     await this.page.click(selectors.LAST_DAYS_A);
     for (let i = 2; i < 20; i++) {
-      this.wait();
-      let links = await this.page.$$(selectors.LIST_PROCESSES)
+      await this.page.waitForSelector(selectors.LIST_PROCESSES);
 
-      debug(links);
-      for (let l in links) {
-        debug(`page: ${i - 1}\tlink: ${l}`);
+      const count = await this.page.evaluate((sel) =>
+        document.querySelectorAll(sel).length, selectors.LIST_PROCESSES);
+
+      for (let l = 0; l < count; l++) {
+        debug(`page: ${i - 1}\tlink: ${l + 1}/${count}`);
         /* old links was destroyed on nav */
-        links = await this.page.$$('a[href*=lnkNumeroProceso]');
-        /* HACK on HACK…
-         * we do this because typescript will complain if we
-         * links[l].click(selectors.JS_CLICK_HACK)
-         */
-        const sel = await links[l].$(selectors.JS_CLICK_HACK);
-        if (sel) {
-          await sel.click();
-          this.wait();
-          this.page.goBack();
-          this.wait();
+        const links = await this.page.$$('a[href*=lnkNumeroProceso]');
+
+        links[l].click();
+        await this.page.waitForSelector(selectors.LIST_FIELDS);
+        const data = [];
+        const fields = await this.page.$$(selectors.LIST_FIELDS);
+        for (let f in fields) {
+          data.push(await fields[f].evaluate(node => [node.id, (node as HTMLElement).innerText]))
         }
+        this.process(data);
+        this.page.goBack();
+        await this.page.waitForSelector(selectors.LIST_PROCESSES);
       };
 
       await this.page.evaluate(`__doPostBack('ctl00$CPH1$GridListaPliegos','Page$${i}')`)
     }
+    this.emit("done");
   }
 }
