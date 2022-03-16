f79f8efcfd0b5eb6b23b101257afe9f703859769
Niv Sardi <xaiki@evilgiggle.com>
2022-03-16 11:44:27 -0300

    puppet: add date to json filename
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- puppet/src/index.ts
+++ puppet/src/index.ts
@@ -1,18 +1,27 @@
 import Runner from './runner';
 import { writeFile } from 'fs';
 
-
 const puppet = new Runner({
   BROWSERLESS_HOST: process.env.BROWSERLESS_HOST || "localhost",
   BROWSERLESS_PORT: process.env.BROWSERLESS_PORT || "3000",
   BASE_URL: process.env.BASE_URL || 'https://www.buenosairescompras.gob.ar'
 });
 
+const z = (d, n=2) => {
+  let s = d.toString();
+  while (s.length < n) {
+    s = '0' + s;
+  }
+  return s;
+}
+
 puppet.on('data', d => {
-  const nump = d.VistaPreviaPliego.CabeceraPliego.NumPliego;
-  writeFile(`${__dirname}/../data/${nump}.json`,
-    JSON.stringify(d, null, 2), err => {
-      if (err) return console.error(`couldn't write file data/${nump}.json: ${err}`);
+    const nump = d.VistaPreviaPliego.CabeceraPliego.NumPliego;
+  const [_, D,M,Y,H] = d.VistaPreviaPliego.Cronograma.FechaPublicacion.match(/(\d+)\/(\d+)\/(\d+) (.*)/);
+  const sdate = `${Y}${z(M)}${z(D)}_${H}`;
+  writeFile(`${__dirname}/../data/${sdate}_${nump}.json`,
+            JSON.stringify(d, null, 2), err => {
+              if (err) return console.error(`couldn't write file data/${nump}.json: ${err}`);
       console.log(`wrote data/${nump}.json`);
 
     })
